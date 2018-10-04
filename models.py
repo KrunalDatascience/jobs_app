734caf0e8df180d218b6c03509ec1df8ffcf645a
Jobanputra <krunal.jobanputra@accenture.com>
2018-10-05 00:35:23 +0530

    first commit

---
+++ models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class JobListing(models.Model):
+    title = models.TextField(null = True, blank = True)
+    description = models.TextField(null = True, blank = True)
---
+++ static/jobs_app/assets/js/our.custom.script.js
@@ -0,0 +1,11 @@
+function showPostForm(){
+    form = $('#addForm');
+    form.css("opacity", "1");
+    form.css("height", "250px");    
+}
+
+function hidePostForm(){
+    form = $('#addForm');
+    form.css("opacity", "0");
+    form.css("height", "0px");    
+}
